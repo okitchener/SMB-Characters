49023a04f4d3c5056c9bdd69000fcd8e3f3f5305
Owen Kitchener <okitchener@my.wctc.wdu>
2025-09-15 14:47:49 -0500

    [1.02] Menu start + nlog test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
+// create instance of Logger
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
+//Barebones menu
+string? choice;
+    do
+    {
+        // display choices to user
+        Console.WriteLine("1) Add Character");
+        Console.WriteLine("2) Display All Characters");
+        Console.WriteLine("Enter to quit");
+
+        // input selection
+        choice = Console.ReadLine();
+        logger.Info("User choice: {Choice}", choice);
+
+        if (choice == "1")
+        {
+            // Add Character
+        }
+        else if (choice == "2")
+        {
+            // Display All Characters
+        }
+    } while (choice == "1" || choice == "2");
